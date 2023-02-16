d7c32fc6374fa30b893769e6e97a49512d5cecef
Nabil <hayounn.etd@gmail.com>
2023-02-16 18:38:30 +0100

    Layout placeholders

--- src/App.js
+++ src/App.js
@@ -2,8 +2,42 @@ import './App.css';
 
 const App = () => {
   return (
-    <div>
-      CRWN Clothing
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-container">
+          <h2>Women</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-container">
+          <h2>Men</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 }
