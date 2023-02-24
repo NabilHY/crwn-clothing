80b10bc4d98d03d1fad83c76f63e7c43f529d6d8
Nabil <hayounn.etd@gmail.com>
2023-02-24 17:56:28 +0100

    Add Firebase SDK

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsw_tUlTynBpCO724PMbGxZRsMaztOfhE",
+  authDomain: "crwn-db-fec7b.firebaseapp.com",
+  projectId: "crwn-db-fec7b",
+  storageBucket: "crwn-db-fec7b.appspot.com",
+  messagingSenderId: "698368632970",
+  appId: "1:698368632970:web:80e553ad0711f4b1902573"
+};
+
+const app = initializeApp(firebaseConfig);
