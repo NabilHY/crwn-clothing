f0e17bccf6a99c8545fbef91c9539d33cee59037
Nabil <hayounn.etd@gmail.com>
2023-02-28 17:55:08 +0100

    set UserContext object on the GoogleSignIn feature

--- src/routes/auth/Auth.component.jsx
+++ src/routes/auth/Auth.component.jsx
@@ -1,18 +1,21 @@
+import { useContext } from 'react';
 import {
   signInWithGooglePopup, 
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 import { SignUpForm } from '../../components/sign-up-form/SignUp.component';
 import { LogIn } from '../../components/log-in/log-in.component';
+import { UserContext } from '../../contexts/user.context';
 import './auth.styles.scss';
 
 export const Auth = () => {
 
+  const { setCurrentUser } = useContext(UserContext);
 
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
+    setCurrentUser(user)
     const userDocRef = await createUserDocumentFromAuth(user);
-    console.log(userDocRef)
   }
 
   return (
