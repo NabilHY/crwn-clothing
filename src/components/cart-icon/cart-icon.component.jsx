02713097fbed0b85aa4b3f2bfbed37de722246ce
Nabil <hayounn.etd@gmail.com>
2023-03-02 12:29:35 +0100

    Toggle Dropdown component onClick

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import { ReactComponent as ShoppingBag } from '../../assets/shopping-bag.svg';
 import { CartDropdown } from '../cart-dropdown/cart-dropdown.component';
 import './cart-icon.styles.scss';
 
 export const CartIcon = () => {
+    const [toggled, setToggled] = useState(false);
+
+    const handleClick = () => {
+        !toggled ? setToggled(true) : setToggled(false);
+    }
+
     return (
         <div className='cart-icon-container'>
-            <ShoppingBag className='shopping-icon' />
-            <span className='item-count'>0</span>
-            <CartDropdown />
-
+            <ShoppingBag className='shopping-icon' onClick={handleClick} />
+            <span className='item-count' onClick={handleClick}>0</span>
+            {
+                toggled && <CartDropdown />
+            }
         </div>
     )
 }
