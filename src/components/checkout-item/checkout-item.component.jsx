80782a65b89f5eacdd70e6145650d35a0e682794
Nabil <hayounn.etd@gmail.com>
2023-03-03 20:26:59 +0100

    Render Checkout component depending on the context

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,5 +1,14 @@
-// export const CheckoutItem = ({item}) => {
-//     return (
+import { useContext } from "react";
+import { setShopContext } from "../../contexts/shop.context";
 
-//     )
-// }
+export const CheckoutItem = ({ item }) => {
+    const {name, imgUrl, price, quantity } = item;
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{imgUrl}</td>
+            <td>{price}</td>
+            <td>{quantity}</td>
+        </tr>
+    )
+}
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,30 +1,40 @@
 import { useContext } from "react";
 import Table from 'react-bootstrap/Table';
-import { ShopContext } from "../../contexts/shop.context";
+import { CartContext } from "../../contexts/cart.context";
 import { CheckoutItem } from "../../components/checkout-item/checkout-item.component";
 
 
 export const Checkout = () => {
 
-    const { shopItems, setShopItems } = useContext(ShopContext);
+    const { cartItems } = useContext(CartContext);
 
     return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>Product</th>
-                    <th>Description</th>
-                    <th>Price</th>
-                    <th>Remove</th>
-                </tr>
-            </thead>
+        <div className="checkout-container">
             {
-                shopItems.map((item) => {
-                    return (
-                        <CheckoutItem item={item} />
-                    )
-                })
+                cartItems.length !== 0 ? (
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Product</th>
+                                <th>Description</th>
+                                <th>Price</th>
+                                <th>Remove</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                cartItems.map((item) => {
+                                    return (
+                                        <CheckoutItem key={item.id} item={item} />
+                                    )
+                            })
+                            }
+                        </tbody>
+                    </Table>
+                ) : (
+                    <p>Your Cart is empty</p>
+                )
             }
-        </Table>
+        </div>
     )
 }
