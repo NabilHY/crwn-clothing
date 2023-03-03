ec41727179d5e064b39c1df44a9730ccaf9acb5e
Nabil <hayounn.etd@gmail.com>
2023-03-03 19:45:37 +0100

    Implment useNavigate hook to redirect to checkout path in CartDropDown component

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,4 +1,10 @@
+import { useContext } from "react";
+import { ShopContext } from "../../contexts/shop.context";
+
 export const Checkout = () => {
+
+    // const [shopItems, setShopItems] = useContext(ShopContext);
+
     return (
         <div>Checkout Compenent</div>
     )
