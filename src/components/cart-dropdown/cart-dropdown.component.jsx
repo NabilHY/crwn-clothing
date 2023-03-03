0dcca457d8bb9989a93f851cef9520c31be0a81a
Nabil <hayounn.etd@gmail.com>
2023-03-03 19:01:16 +0100

    Render CartDropdown component depending on cartItens context

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { Fragment, useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 import { Button } from "../button/button.component";
 import { CartDropdownItem } from "../cart-dropdown-item/cart-dropdown-item.component";
@@ -6,16 +6,25 @@ import "./cart-dropdown.styles.scss";
 
 export const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
-
+  console.log(cartItems.length);
   return (
     <div className="cart-dropdown-container">
-      <div>
-        {cartItems &&
-          cartItems.map((item) => {
-            return <CartDropdownItem key={item.id} item={item} />;
-          })}
-      </div>
-      <Button>Go to Checkout</Button>
+      {
+        cartItems.length !== 0 ? (
+        <Fragment>
+          <div>
+            {cartItems &&
+              cartItems.map((item) => {
+                return <CartDropdownItem key={item.id} item={item} />;
+              })}
+          </div>
+          <Button>Go to Checkout</Button>
+        </Fragment>
+      
+      ) : (
+        <p>Cart Is empty</p>           
+      )
+      }
     </div>
   );
 };
