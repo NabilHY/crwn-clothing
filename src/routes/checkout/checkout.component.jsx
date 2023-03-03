867507fd9c287c2c472471b8115c76547ee4fa9a
Nabil <hayounn.etd@gmail.com>
2023-03-03 19:56:35 +0100

    Import Table wrapper

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,21 +1,16 @@
 import { useContext } from "react";
+import Table from 'react-bootstrap/Table';
 import { ShopContext } from "../../contexts/shop.context";
 import { CheckoutItem } from "../../components/checkout-item/checkout-item.component";
+
 
 export const Checkout = () => {
 
     const { shopItems, setShopItems } = useContext(ShopContext);
 
     return (
-        <div className='checkout-container'>
-            {
-                shopItems &&
-                shopItems.map((item) => {
-                    return (
-                        <CheckoutItem item={item} />
-                    )
-                })
-            }
-        </div>
+        <Table>
+            
+        </Table>
     )
 }
