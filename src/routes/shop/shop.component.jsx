099f6526fa87c6c95189b1cc4728020d10d333fb
Nabil <hayounn.etd@gmail.com>
2023-03-30 10:45:52 +0000

    Limit products shown to 4 and add button to redirect

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,6 +2,8 @@ import React, { Fragment } from "react";
 import { useContext } from "react";
 import { ShopContext } from "../../contexts/shop.context";
 import { Product } from "../../components/product/product.component";
+import { Button } from "../../components/button/button.component";
+
 import "./shop.styles.scss";
 
 export const Shop = () => {
@@ -13,10 +15,13 @@ export const Shop = () => {
           <Fragment key={category}>
             <h2>{category.toUpperCase()}</h2>
             <div className="products-container">
-              {shopCategories[category].map((product) => (
+              {shopCategories[category].slice(0,4).map((product) => (
                 <Product product={product} key={product.id} />
               ))}
             </div>
+            <Button type="button" buttonType='inverted'>
+              View More
+            </Button>
           </Fragment>
         ))}
       </>
