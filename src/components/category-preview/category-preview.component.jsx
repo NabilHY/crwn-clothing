e89cf00200d68297179c53493320f77da4625cc8
Nabil <hayounn.etd@gmail.com>
2023-03-30 12:28:08 +0000

    Create a category preview component to list products for each category

---
+++ src/components/category-preview/category-preview.component.jsx
@@ -0,0 +1,24 @@
+import { Product } from '../product/product.component';
+import './category-preview.styles.scss';
+
+import React from 'react'
+
+const CategoryPreview = ({title, items}) => {
+  return (  
+      <div className='category-preview-container'>
+          <h2>
+              <span className='title'>{title.toUpperCase()}</span>
+          </h2>
+          <div className="preview">
+              {
+                  items.filter((_, idx) => idx < 4)
+                      .map((product) => 
+                          <Product key={product.id} product={product} />
+                  )
+              }
+          </div>
+      </div>
+  )
+}
+
+export default CategoryPreview
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,27 +3,18 @@ import { useContext } from "react";
 import { ShopContext } from "../../contexts/shop.context";
 import { Product } from "../../components/product/product.component";
 import { Button } from "../../components/button/button.component";
-
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 import "./shop.styles.scss";
 
 export const Shop = () => {
   const { shopCategories } = useContext(ShopContext);
 
   return (
-      <>
-        {Object.keys(shopCategories).map((category) => (
-          <Fragment key={category}>
-            <h2>{category.toUpperCase()}</h2>
-            <div className="products-container">
-              {shopCategories[category].slice(0,4).map((product) => (
-                <Product product={product} key={product.id} />
-              ))}
-            </div>
-            <Button type="button" buttonType='inverted'>
-              View More
-            </Button>
-          </Fragment>
-        ))}
-      </>
+      <div className='shop-container'>
+        {Object.keys(shopCategories).map((category) => {
+          const products = shopCategories[category];
+          return <CategoryPreview key={category} title={category} items={products} />
+        })}
+      </div>
   );
 };
